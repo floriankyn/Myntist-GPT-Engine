8a07a9daac1eed91ce936109ec8c78fea124808f
Florian Lepage <florian.kyn@gmail.com>
2023-10-25 16:34:05 +0200

    Update vectors.ts

--- src/api/v1/routes/vectors.ts
+++ src/api/v1/routes/vectors.ts
@@ -3,11 +3,11 @@ const router = express.Router();
 
 import { authenticate } from "../middleware/authentication.js";
 import { save, compute, list } from "../controller/vectors.js";
-import { validateCreateVector } from "../validator/vectors.js";
+import { validateCreateVector, validateComputeVector } from "../validator/vectors.js";
 
 
 router.post("/api/v1/vectors/save", [authenticate, validateCreateVector], save);
-router.post("/api/v1/vectors/compute", [authenticate], compute);
+router.post("/api/v1/vectors/compute", [authenticate, validateComputeVector], compute);
 router.get("/api/v1/vectors/list/:source", [authenticate], list);
 //router.delete("/api/v1/vectors/delete", [authenticate], remove);
 
