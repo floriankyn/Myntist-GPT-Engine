f01b70610a657e6ea43aa1ebb7b6b80ee09494be
Florian Lepage <florian.kyn@gmail.com>
2023-10-25 16:11:03 +0200

    Update vectors.ts

--- src/api/v1/validator/vectors.ts
+++ src/api/v1/validator/vectors.ts
@@ -2,7 +2,14 @@ import Joi from "joi";
 
 export const validateCreateVector = async (req, res, next) => {
     const createVectorValidation = Joi.object({
-        input: Joi.string().required(),
+        input: Joi.array()
+            .items(
+                Joi.object({
+                    id: Joi.string().required(),
+                    content: Joi.string().required(),
+                })
+            )
+            .required(),
     });
 
     const { error } = createVectorValidation.validate(req.body);
