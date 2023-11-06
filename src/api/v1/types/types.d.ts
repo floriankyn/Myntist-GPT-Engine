12de7a8f3db84122c891d3501e1b048bb5b3e757
Florian Lepage <florian.kyn@gmail.com>
2023-11-06 16:58:13 +0100

    Remove unused type

--- src/api/v1/types/types.d.ts
+++ src/api/v1/types/types.d.ts
@@ -2,11 +2,3 @@ interface vectorSave {
   id: string;
   content: string;
 }
-
-interface vectorResult {
-  ids: string[][];
-  distances: number[][];
-  metadatas: null[][];
-  embeddings: null;
-  documents: string[][];
-}
